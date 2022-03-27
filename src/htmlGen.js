1b612b8db4f1db7e083d3050337d34f9b5c1f3c0
The Halfrican <noahmattocks@gmail.com>
2022-03-27 14:45:59 -0700

    Added style with CSS and added tests

--- src/htmlGen.js
+++ src/htmlGen.js
@@ -1,52 +1,76 @@
 const generateManagerCard = (manager) => {
+   
     return `
-        <div class="card">
-            <h3>${manager.getName()}</h3>
-                <p><i class="fa-solid fa-mug-hot"></i>${manager.getRole()}</p>
-                <div class="card-body">
-                <ul class="list-group list-group-flush">
-                    <li class="list-group-item">${manager.getId()}</li>
-                    <li class="list-group-item">Email: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a>
-                    </li>
-                    <li class="list-group-item">Office number: ${manager.getOfficeNumber()}</li>
-                </ul>
-            </div>
-        </div>
+    
+    <div class="card" style="width: 18rem;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">${manager.getName()}</h5>
+    <p class="card-text">${manager.getRole()}</p>
+  </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">Manager Id: ${manager.getId()}</li>
+    <li class="list-group-item">Email: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></li>
+    <li class="list-group-item">Office number: ${manager.getOfficeNumber()}</li>
+  </ul>
+  <div class="card-body">
+    <a href="#" class="card-link">Card link</a>
+    <a href="#" class="card-link">Another link</a>
+  </div>
+</div>
+    
     `;
+
+
 };
 
 const generateEngineerCard = (engineer) => {
+  
     return `
-        <div class="card">
-            <h3>${engineer.getName()}</h3>
-                <p><i class="fa-solid fa-mug-hot"></i>${engineer.getRole()}</p>
-                <div class="card-body">
-                <ul class="list-group list-group-flush">
-                    <li class="list-group-item">${engineer.getId()}</li>
-                    <li class="list-group-item">Email: <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a>
-                    </li>
-                    <li class="list-group-item">GitHub: ${engineer.getGithub()}</li>
-                </ul>
-            </div>
-        </div>
+    
+    <div class="card" style="width: 18rem;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">${engineer.getName()}</h5>
+    <p class="card-text">${engineer.getRole()}</p>
+  </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">Engineer Id: ${engineer.getId()}</li>
+    <li class="list-group-item">Email: <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a></li>
+    <li class="list-group-item">GitHub: ${engineer.getGithub()}</li>
+  </ul>
+  <div class="card-body">
+    <a href="#" class="card-link">Card link</a>
+    <a href="#" class="card-link">Another link</a>
+  </div>
+</div>
+    
     `;
 };
 
 const generateInternCard = (intern) => {
+
     return `
-        <div class="card">
-            <h3>${intern.getName()}</h3>
-                <p><i class="fa-solid fa-mug-hot"></i>${intern.getRole()}</p>
-                <div class="card-body">
-                <ul class="list-group list-group-flush">
-                    <li class="list-group-item">${intern.getId()}</li>
-                    <li class="list-group-item">Email: <a href="mailto:${intern.getEmail()}">${intern.getEmail()}</a>
-                    </li>
-                    <li class="list-group-item">School: ${intern.getSchool()}</li>
-                </ul>
-            </div>
-        </div>
+    
+    <div class="card" style="width: 18rem;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">${intern.getName()}</h5>
+    <p class="card-text">${intern.getRole()}</p>
+  </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">Engineer Id: ${intern.getId()}</li>
+    <li class="list-group-item">Email: <a href="mailto:${intern.getEmail()}">${intern.getEmail()}</a></li>
+    <li class="list-group-item">School: ${intern.getSchool()}</li>
+  </ul>
+  <div class="card-body">
+    <a href="#" class="card-link">Card link</a>
+    <a href="#" class="card-link">Another link</a>
+  </div>
+</div>
+    
     `;
+    
 };
 
 const baseHtml = (htmlArr) => `
@@ -57,7 +81,7 @@ const baseHtml = (htmlArr) => `
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="../assets/style.css">
+    <link rel="stylesheet" type="text/css" href="../assets/css/style.css">
     <title>Document</title>
 </head>
 <body>
